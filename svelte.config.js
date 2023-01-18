f30710f97d755a90e53f766fdb92eebc9a7cc343
moamenhredeen <moamenhredeen@gmail.com>
2023-01-18 06:32:55 +0100

    fix base url

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
     adapter: adapter()
   },
   paths: {
-    base: process.env.NODE_ENV === "production" ? "/moamenhredeen.github.io/portfolio/" : ""
+    base: "moamenhredeen.github.io/portfolio/"
   }
 };
 
