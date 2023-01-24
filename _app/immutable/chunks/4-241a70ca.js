1ab69060d79d1bcd26eef6dbf74594fdc12d8bf1
moamenhredeen <moamenhredeen@gmail.com>
2023-01-24 05:14:57 +0100

    Updates

--- _app/immutable/chunks/4-241a70ca.js
+++ _app/immutable/chunks/4-241a70ca.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-90a7f2f4.js";import{default as t}from"../components/pages/cv/_page.svelte-b4afb0c6.js";export{t as component,r as universal};
+import{_ as r}from"./_page-e94d8ac7.js";import{default as t}from"../components/pages/cv/_page.svelte-b4afb0c6.js";export{t as component,r as universal};
