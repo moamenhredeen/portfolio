66a8d84d1ea2fe5da925c4a276fb8cbda053f61d
moamen.hraden <moamen.hraden@primion.eu>
2023-10-06 21:08:15 +0200

    update

--- src/routes/blog/[slug]/+layout.svelte
+++ src/routes/blog/[slug]/+layout.svelte
@@ -1,22 +1,25 @@
-
 <script>
 
-    const  disqus_config = function () {
-        this.page.url = 'moamenhredeen.github.io';
-        this.page.identifier = 'moamenhredeen-github-io';
-    };
+    import {onMount} from "svelte";
 
-    (function() {
-        const d = document, s = d.createElement('script');
-        s.src = 'https://moamenhredeen-github-io.disqus.com/embed.js';
-        s.setAttribute('data-timestamp', + new Date());
-        (d.head || d.body).appendChild(s);
-    })();
+    onMount(() => {
+        var disqus_config = function () {
+            this.page.url = 'moamenhredeen.github.io';
+            this.page.identifier = 'moamenhredeen-github-io';
+        };
+
+        (function () {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://moamenhredeen-github-io.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    });
 
 </script>
 
 <article class="p-4 m-auto w-full xl:w-1/2">
-    <slot />
+    <slot/>
 </article>
 <div id="disqus_thread"
      class="my-12 w-full xl:w-1/2 m-auto"></div>
