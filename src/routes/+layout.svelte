ea6f71c7b7ecc72e5c48a7cac47ec27dd4c22164
Moamen Hredeen <moamen@hredeen.com>
2025-04-13 06:14:05 +0200

    add nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,14 +8,43 @@
 	let { children }: LayoutProps = $props();
 </script>
 
+<ul>
+	<li><a href="/about" class:active={page.url.pathname === '/about/'}>ABOUT</a></li>
+	<li><a href="/blog" class:active={page.url.pathname.startsWith('/blog/')}>BLOG</a></li>
+	<li><a href="/" class:active={page.url.pathname === '/'}>HOME</a></li>
+</ul>
+
 {#key page.url.pathname}
-	<main  in:fly={{ x: -200, duration: 500 }}>
+	<main  in:fly={{ y: 200, duration: 500 }}>
 		{@render children()}
 	</main>
 {/key}
 
 
 <style>
+		ul{
+				position: fixed;
+				left: 0;
+				top: 0;
+				list-style: none;
+				padding: 0;
+				display: flex;
+				gap: 4rem;
+				transform: rotate(-90deg) translate(-100%, -200%);
+
+				a{
+						display: block;
+						font-size: 1.2rem;
+						text-decoration: none;
+						color: inherit;
+						transition: 0.3s;
+				}
+
+				a.active{
+						transform: scale(1.5);
+				}
+		}
+
 	main{
       max-width: 800px;
 	}
