9a43eb340cf60e103089ce00c15a77b9e20467c4
Moamen Hredeen <moamen@hredeen.com>
2025-06-02 10:22:55 +0200

    remove svelte files

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment';
-
-export const title = "Moamen Hredeen"
-export const description = "Moamen Hredeen Personal Portfolio"
-export const url = dev ? "http://localhost:5173" : "http://moamenhredeen.me"
